79738086d775cf7dc62b43e1d168bb3fe10649f7
aze-ish <hazelannreptin.obidece24@bicol-u.edu.ph>
2026-04-20 17:00:29 +0800

    Created dataRoutes.js with DELETE API

---
+++ backend/routes/dataRoutes.js
@@ -0,0 +1,8 @@
+﻿const express = require('express');
+const router = express.Router();
+const protect = require('../middleware/authMiddleware');
+const dataController = require('../controllers/dataController');
+
+router.delete('/wipe', protect, dataController.wipeUserData);
+
+module.exports = router;
